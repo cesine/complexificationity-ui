24c052909b4e704a58a5c51062afa271e89443b5
cesine <cesine@yahoo.com>
2018-01-10 13:38:38 +0000

    using cors request if on file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-fetch('/v1/codebases').then(function(response) {
+const url =  window.location.protocol === 'file:' ? 'https://localhost:8010': '';
+
+fetch(url + '/v1/codebases').then(function(response) {
   var codebases = new CodeBases({
     data: response.json(),
     element: document.getElementById("codebases-scatterplot")
